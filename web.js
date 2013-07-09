0763d912604ebda34ef34090dd4b6d1120b021ab
Martin Henderson <martin_henderson@hotmail.com>
2013-07-09 20:26:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var html = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
