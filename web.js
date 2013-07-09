782386e3b1a880789ed2259396a859419e421654
Martin Henderson <martin_henderson@hotmail.com>
2013-07-09 20:55:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var html = fs.readFileSync('index.html');
+var html = fs.readFileSync('index.html','utf-8');
 
 var buffer = new Buffer(256);
 buffer.write(html, "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
